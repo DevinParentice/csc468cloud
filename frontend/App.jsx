c58149ca7d0d013aed50a3bc4317764727a9f279
Devin Parentice <devinparentice@gmail.com>
2022-02-10 21:56:37 -0500

    Fixed folder structure

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./styles/App.css";
 
 function App() {
-
 	return (
 		<div className="App">
 			<h1>Chess Game</h1>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
